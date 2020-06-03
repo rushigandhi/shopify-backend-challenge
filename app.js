051b2d390aa73249f10668ccb86eafda64fc70be
Rushi Gandhi <rushi.gandhi@hotmail.com>
2020-06-03 01:16:10 -0400

    Created structure for integration and unit tests

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+// import all the dependencies
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var dotenv = require("dotenv");
 dotenv.config();
+
+// router imports
 var indexRouter = require("./routes/index");
 var imagesRouter = require("./routes/images.route");
 var usersRouter = require("./routes/users.route");
 require("./middleware/passport");
 
+// start express
 var app = express();
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
+// user routers
 app.use("/", indexRouter);
 app.use("/images", imagesRouter);
 app.use("/users", usersRouter);
--- utils/checkOwner.js
+++ utils/checkOwner.js
@@ -1,5 +1,6 @@
 const { getImage } = require("../db/image.db");
 
+// Helper util function that returns whether the User owns the image
 const isOwner = async (userId, imageId) => {
   const image = await getImage(imageId);
   return image.userId == userId;
